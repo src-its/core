29a584a32e2b686985c119ec817bc2c4f30d8431
jfoos <jfoos14@simons-rock.edu>
2016-05-12 16:49:17 -0400

    created a homepage template and directed view to it

--- ca_wiki/views.py
+++ ca_wiki/views.py
@@ -1,7 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.template import RequestContext, loader
+
+from .models import Issue
 
 def index(request):
-    return HttpResponse("Welcome to the CA Wiki. Here's some text while we \
-            figure out how to serve a page.")
+    new_issues = Issue.objects.order_by('-issue_opened')
+    template = loader.get_template('ca_wiki/index.html')
+    context = RequestContext(request, {
+            'new_issues': new_issues,
+    })
+    return HttpResponse(template.render(context))
+    # return HttpResponse("Welcome to the CA Wiki. Here's some text while we \
+    #        figure out how to serve a page.")
     
